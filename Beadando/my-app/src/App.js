f6b494afdb2cc49722bb8e6ad50c29af3ea1a2c4
Túri Balázs <106369596+TuriBalazs@users.noreply.github.com>
2026-03-09 08:50:53 +0100

    begining

---
+++ Beadando/my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Web programozás-1 Előadás Házi feladat</h1>
+      </header>
+      <footer>
+        <p>Túri Balázs - GJ8N08</p>
+        <p>Kocsis Ágnes Virág - XNNBSI</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
