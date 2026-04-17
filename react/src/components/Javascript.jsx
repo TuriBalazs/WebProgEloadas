3684acc90ba4960b872aeb36a131ed7a4280bd00
Túri Balázs <106369596+TuriBalazs@users.noreply.github.com>
2026-04-17 07:33:06 +0200

    dokumentáció bővítése

--- react/src/components/Javascript.jsx
+++ react/src/components/Javascript.jsx
@@ -4,29 +4,18 @@ function Javascript() {
             <div class="employee-form">
                 <form onsubmit="event.preventDefault();onFormSubmit();">
                     <div>
-                        <label>Full Name*</label><label class="validation-error hide" id="fullNameValidationError">This field is required.</label>
-                        <input type="text" name="fullName" id="fullName"/>
+                        <label>Kerület</label>
+                        <input type="text" name="kerulet" id="kerulet"/>
                     </div>
                     <div>
-                        <label>Email Id</label>
-                        <input type="text" name="email" id="email"/>
-                    </div>
-                    <div>
-                        <label>Salary</label>
-                        <input type="text" name="salary" id="salary"/>
-                    </div>
-                    <div>
-                        <label>City</label>
-                        <input type="text" name="city" id="city"/>
-                    </div>
-                    <div class="form-action-buttons">
-                        <input type="submit" value="Submit"/>
+                        <label>Cím</label>
+                        <input type="text" name="cim" id="cim"/>
                     </div>
                 </form>
             </div>
             <br />
             <div className="overflow-x-auto">
-                <table className="table table-xs" id="employeeList">
+                <table className="table table-xs" id="hely">
                     <thead>
                         <tr>
                             <th>Kerület</th>
